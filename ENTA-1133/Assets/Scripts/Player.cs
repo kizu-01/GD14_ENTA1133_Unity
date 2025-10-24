c94cd6e0ca33964e37a6b0f8f4fccfe46b84db2e
kizu-01 <demicadelinia.personal@gmail.com>
2025-10-23 23:59:46 -0700

    Lab 8 - Unity Integration
    Brought my Dice Game (One Round) logic into Unity and replaced Console inputs with Debug log.

---
+++ ENTA-1133/Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GD14_1133_Lab3_DiceGame_Cadelinia_Demi.Scripts
+{
+    internal class Player
+    {
+        public string Name { get; private set; }
+        public int CurrentRoll { get; set; }
+        public int Score { get; set; }
+
+        public Player(string name)
+        {
+            Name = name;
+            Score = 0;
+            CurrentRoll = 0;
+        }
+
+        // Convenience: roll a specific die using a Die instance and store the result.
+        public int RollWithDie(int sides, Die die)
+        {
+            if (die == null) throw new ArgumentNullException(nameof(die));
+            CurrentRoll = die.Roll(sides);
+            return CurrentRoll;
+        }
+    }
+}
