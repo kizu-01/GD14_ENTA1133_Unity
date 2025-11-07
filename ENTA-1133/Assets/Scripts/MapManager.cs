7e9772142ab1b15acd67eaa5979aa1df4e97b7bd
kizu-01 <demicadelinia.personal@gmail.com>
2025-11-07 04:40:57 -0800

    Lab 10 - Room Design & Map Generation

--- ENTA-1133/Assets/Scripts/MapManager.cs
+++ ENTA-1133/Assets/Scripts/MapManager.cs
@@ -6,7 +6,7 @@ public class MapManager : MonoBehaviour
     [SerializeField] private float RoomSize = 1;
     [SerializeField] private int MapSize = 3;
     private RoomBaseMono[,] _map;
-    
+
     public void CreateMap()
     {
         _map = new RoomBaseMono[MapSize, MapSize];
@@ -14,13 +14,26 @@ public class MapManager : MonoBehaviour
         {
             for (int z = 0; z < MapSize; z++)
             {
-                Vector2 coords = new Vector2(x * RoomSize, z * RoomSize);
+                Vector3 coords = new Vector3(x * RoomSize, 3, z * RoomSize);
 
                 var roomInstance = Instantiate(RoomPrefabs[Random.Range(0, RoomPrefabs.Length)], transform);
 
                 roomInstance.transform.position = coords;
 
                 _map[x, z] = roomInstance;
+            }
+        }
+        for (int x = 0; x < MapSize; x++)
+        {
+            for (int z = 0; z < MapSize; z++)
+            {
+                RoomBaseMono currentRoom = _map[x, z];
+                RoomBaseMono north = null, south = null, east = null, west = null;
+                if (x > 0) east = _map[x - 1, z];
+                if (x < MapSize - 1) west = _map[x + 1, z];
+                if (z > 0) north = _map[x, z - 1];
+                if (z < MapSize - 1) south = _map[x, z + 1];
+                currentRoom.SetRooms(north, south, east, west);
             }
         }
     }
