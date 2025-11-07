8b27d464e028db86edae2ea4001efbec289191d3
kizu-01 <demicadelinia.personal@gmail.com>
2025-11-06 20:45:10 -0800

    Fixing files error

---
+++ ENTA-1133/Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private MapManager GameMapPrefab;
+
+    private MapManager _gameMap;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void Start()
+    {
+        Debug.Log("GameManager Start");
+        // 
+        transform.position = Vector3.zero;
+
+        // create an instance
+        _gameMap = Instantiate(GameMapPrefab, transform);
+        _gameMap.transform.position = Vector3.zero;
+
+        Debug.Log("GameManager Map Created");
+
+    }
+    private void StartGame()
+    {
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
